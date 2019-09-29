0af95af125c3be5f14ba03367ba12110117c8019
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-09-29 11:35:50 -0300

    Creating a functional component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
       <h1>Javascript is awesome!</h1>
+      <p>works! =)</p>
+      <Person/>
     </div>
   );
 }
