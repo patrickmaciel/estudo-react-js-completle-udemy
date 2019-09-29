90c9de73f38baa7d5506c805881da9ed5ae38419
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-09-29 12:42:26 -0300

    Passing method references between components

---
+++ src/App-useState-Hook.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+// Class base Component or Functional Use Hook 
+// Smart component (not dumb ar stateless components)
+
+// React Hooks - useState is a Hook
+
+// Isso é um Functional component
+// useState permite que eu adicione o recurso de States (estados) em um componente funcional.
+
+// This is a functional component
+// useState allow us to add the state feature to functional components
+const App = props => {
+  const [ personsState, setPersonsState ] = useState(
+    {
+      persons: [
+        {name: 'Patrick', age: 30, hobbies: 'GW2'},
+        {name: 'Aline', age: 31, hobbies: 'Patins'},
+        {name: 'Nycole', age: 14, hob0bies: 'Japa'},
+      ]
+    }
+  );
+
+  const [otherState, setOtherState] = useState('some other value');
+
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
+    setPersonsState({
+      persons: [
+        {name: 'Patrick Maciel', age: 30, hobbies: 'GW2'},
+        {name: 'Aline Martins', age: 31, hobbies: 'Patins'},
+        {name: 'Nycole Martins', age: 14, hobbies: 'Japa'},
+      ],
+      // otherState: personsState.otherState // reddux not merge the old state, but replace it
+    })
+  };
+
+  return (
+    <div className="App">
+      <h1>Javascript is awesome!</h1>
+      <p>works! =)</p>
+
+      <button onClick={switchNameHandler}>Switch Name</button>
+
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>{personsState.persons[0].hobbies}</Person>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>{personsState.persons[1].hobbies}</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>{personsState.persons[2].hobbies}</Person>
+    </div>
+  );
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,55 +1,57 @@
-import React, { useState } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-// Class base Component or Functional Use Hook 
-// Smart component (not dumb ar stateless components)
+class App extends Component {
+  state = {
+    persons: [
+      {name: 'Patrick', age: 30, hobbies: 'GW2'},
+      {name: 'Aline', age: 31, hobbies: 'Patins'},
+      {name: 'Nycole', age: 14, hobbies: 'Japa'},
+    ],
+    otherState: 'some other value'
+  }
 
-// React Hooks - useState is a Hook
-
-// Isso é um Functional component
-// useState permite que eu adicione o recurso de States (estados) em um componente funcional.
-
-// This is a functional component
-// useState allow us to add the state feature to functional components
-const App = props => {
-  const [ personsState, setPersonsState ] = useState(
-    {
+  switchNameHandler = (newName) => {
+    this.setState({
       persons: [
-        {name: 'Patrick', age: 30, hobbies: 'GW2'},
-        {name: 'Aline', age: 31, hobbies: 'Patins'},
-        {name: 'Nycole', age: 14, hob0bies: 'Japa'},
-      ]
-    }
-  );
-
-  const [otherState, setOtherState] = useState('some other value');
-
-  console.log(personsState, otherState);
-
-  const switchNameHandler = () => {
-    setPersonsState({
-      persons: [
-        {name: 'Patrick Maciel', age: 30, hobbies: 'GW2'},
+        {name: newName, age: 30, hobbies: 'GW2'},
         {name: 'Aline Martins', age: 31, hobbies: 'Patins'},
         {name: 'Nycole Martins', age: 14, hobbies: 'Japa'},
-      ],
-      // otherState: personsState.otherState // reddux not merge the old state, but replace it
+      ]
     })
-  };
+  }
 
-  return (
-    <div className="App">
-      <h1>Javascript is awesome!</h1>
-      <p>works! =)</p>
+  render() {
+    return (
+      <div className="App">
+        <h1>Javascript is awesome!</h1>
+        <p>works! =)</p>
 
-      <button onClick={switchNameHandler}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler('Opa')}>Switch Name</button>
 
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>{personsState.persons[0].hobbies}</Person>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>{personsState.persons[1].hobbies}</Person>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>{personsState.persons[2].hobbies}</Person>
-    </div>
-  );
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} 
+          click={this.switchNameHandler.bind(this, 'Servo de Deus')}>
+          {this.state.persons[0].hobbies}
+        </Person>
+
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}>
+          {this.state.persons[1].hobbies}
+        </Person>
+
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}>
+          {this.state.persons[2].hobbies}
+        </Person>
+
+      </div>
+    );
+  }
 }
 
 export default App;
